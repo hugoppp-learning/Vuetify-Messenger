a9fb2083b2c65454283886cd9ba6e9d337edd02b
Hugo Protsch <hugo@protsch.dev>
2022-08-27 22:47:05 +0200

    Initial

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <HelloWorld></HelloWorld>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="display-2 font-weight-bold mb-3">
+    profile
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Profile'
+}
+</script>
+
+<style scoped>
+
+</style>
