38a4c74a44b1c9d86f393fa89640125ed2ef003f
Hugo Protsch <hugo@protsch.dev>
2022-08-28 11:45:12 +0200

    Add messages

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -22,6 +22,37 @@
           rounded="lg"
         >
           <!--  -->
+
+          <v-list>
+            <v-card v-for="post in posts" class="ma-5 pa-3">
+
+              <div class="d-flex">
+
+                <div class="pr-2">
+                  <v-avatar size="48">
+                    <img :src="post.profilePicture" alt="">
+                  </v-avatar>
+                </div>
+
+                <div class="pt-1 px-1">
+                  @<span class="font-weight-bold">{{ post.name }}</span>
+
+                  <div class="pl-0 py-1">{{ post.message }}</div>
+
+
+                  <div class="d-flex" style="align-items: center">
+                    <v-icon class="mr-2">mdi-heart-outline</v-icon>
+                    <span>{{ post.likes }}</span>
+                  </div>
+                </div>
+              </div>
+            </v-card>
+
+
+          </v-list>
+
+
+          <!--  -->
         </v-sheet>
       </v-col>
 
@@ -49,6 +80,53 @@ export default {
 
   components: {
     HelloWorld
-  }
+  },
+
+  data: () => ({
+    posts: [
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: 'This is the first post on this site, Yay!! :)',
+        likes: 12
+      },
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: 'This is the second post on this site',
+        likes: 1
+      },
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+        likes: 0
+      },
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+        likes: 0
+      },
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+        likes: 0
+      },
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+        likes: 0
+      },
+      {
+        name: 'hugop',
+        profilePicture: 'https://i.pravatar.cc/300',
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+        likes: 0
+      },
+    ]
+  }),
 }
 </script>
